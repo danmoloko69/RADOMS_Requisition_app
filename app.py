5890ba993b741a307c3b25eb4c06559721f9b0fe
danmoloko69 <danmoloko69@gmail.com>
2026-05-09 18:11:23 +0200

    commit

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ def walletconnect_component():
     </html>
     """
 
-    return components.html(wallet_html, height=400)
+    return components.html(wallet_html, height=500)
 
 def has_metamask():
     try:
